7ed33986e907e73ca2ca8b2ea71780c165a36944
cryptopotluck <cryptopotluck@gmail.com>
2018-12-29 14:24:57 -0600

    Third commit

--- btrealestate/settings.py
+++ btrealestate/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'btredb',
         'USER': 'postgres',
-        'PASSWORD': '',
+        'PASSWORD': '15a14l1k',
         'HOST': 'localhost'
     }
 }
@@ -150,5 +150,5 @@ MESSAGE_TAGS = {
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'austinkiese@gmail.com'
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_PASSWORD = '12a10l1k'
 EMAIL_USE_TLS = True
